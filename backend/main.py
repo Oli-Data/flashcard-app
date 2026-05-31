35e645d4c1f22392842220e532d69f9b45d4def7
Christian Olivares-Rodriguez <69546302+Oli-Data@users.noreply.github.com>
2026-05-31 14:17:11 -0700

    Add lumitodee.vercel.app to CORS

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
         "http://localhost:5173",
         "https://flashcard-mrnsrjxq6-christian-olivares-rodriguezs-projects.vercel.app",
         "https://flashcard-app.vercel.app",
+        "https://lumitodee.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
